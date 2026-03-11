0ec0f15fc80223475fe9f063c26693eebfc352f7
Davli4 <vladislauskirey@yandex.by>
2026-03-12 02:19:50 +0300

    feat: Добавлены entities

---
+++ src/main/java/com/example/bankcards/entity/model/Role.java
@@ -0,0 +1,22 @@
+package com.example.bankcards.entity.model;
+
+import com.example.bankcards.entity.enums.RoleName;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true, length = 20)
+    private RoleName name;
+}
